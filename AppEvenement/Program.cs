a4c31872c2758add839d5cc29f2173bc37390c23
Antoine Chaumet <desert@Huawei-CH>
2024-05-15 17:14:03 +0200

    Init bin

--- AppEvenement/Program.cs
+++ AppEvenement/Program.cs
@@ -16,7 +16,7 @@ namespace AppEvenement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmAccueil());
         }
     }
 }
